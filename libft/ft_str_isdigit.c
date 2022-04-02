646def025c048780197f3b4443f3692d799342d8
elraira- <elraira-@student.42sp.org.br>
2022-04-02 20:20:14 -0300

    bug fixed (+ and -)

--- libft/ft_str_isdigit.c
+++ libft/ft_str_isdigit.c
@@ -6,19 +6,20 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 22:54:30 by elraira-          #+#    #+#             */
-/*   Updated: 2022/04/02 19:50:49 by elraira-         ###   ########.fr       */
+/*   Updated: 2022/04/02 20:10:55 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 int	ft_str_isdigit(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' && ft_isdigit(str[i + 1]))
+		i++;
+	if (str[i] == '+' && ft_isdigit(str[i + 1]))
 		i++;
 	while (str[i])
 	{
